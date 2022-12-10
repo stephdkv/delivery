ded9f2b4140aba4c60c3d21b18e76e4f5617da6f
ab <a.biryukov@itgrade.ru>
2022-12-10 11:58:52 +0300

    refactor models.py

--- data_base.py
+++ data_base.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
+import settings
 
-engine = create_engine('postgresql://hsofztuz:TBVw2WlZGLcrZGOb2YC5h-oQw-Sw8dXo@mouse.db.elephantsql.com/hsofztuz')
-data_base_session = scoped_session(sessionmaker(bind = engine))
+engine = create_engine(f'postgresql://{settings.LOGIN}:{settings.PASSWORD}@{settings.POSTGRES_URL}/{settings.DB_NAME}')
+data_base_session = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
 Base.query = data_base_session.query_property()
--- models.py
+++ models.py
@@ -1,77 +1,80 @@
 from sqlalchemy import (Column, Integer, String, DECIMAL,
-                         Date, ForeignKey, Boolean, Text)
+                        Date, ForeignKey, Boolean, Text)
 from sqlalchemy.orm import relationship
 from data_base import Base, engine
 
+
 class User(Base):
-    __tablename__ = 'user'
-    id = Column(Integer, primary_key = True)
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
     first_name = Column(String())
     last_name = Column(String())
     birthday = Column(Date())
     phone = Column(String(11))
-    email = Column(String(120), unique = True)
-    registred_day = Column(Date())
-    addresses = relationship("Address")
-    orders = relationship("Order")
-    baskets = relationship("Basket")
+    email = Column(String(120), unique=True)
+    register_day = Column(Date())
+    addresses = relationship('Address')
+    orders = relationship('Order')
+    baskets = relationship('Basket')
 
     def __repr__(self):
-        return f"Client {self.id}, {self.full_name}"
+        return f'Client {self.id}, {self.full_name}'
+
 
 class Address(Base):
-    __tablename__ = 'address'
-    id = Column(Integer, primary_key = True)
+    __tablename__ = 'addresses'
+    id = Column(Integer, primary_key=True)
     city = Column(String(30))
     street = Column(String())
     house = Column(String())
     apartment = Column(String())
-    user_id = Column(Integer, ForeignKey('user.id'))
-    deliveries = relationship("Delivery")
+    user_id = Column(Integer, ForeignKey('users.id'))
+    deliveries = relationship('Delivery')
+
 
 class Product(Base):
-    __tablename__ = 'product'
-    id = Column(Integer, primary_key = True)
+    __tablename__ = 'products'
+    id = Column(Integer, primary_key=True)
     title = Column(String())
     price = Column(Integer())
     description = Column(Text())
     calories = Column(String())
-    baskets = relationship("Basket_product", backref = 'product')
-    categories = relationship("Category", secondary = "product_category")
+    baskets = relationship('BasketProduct', backref='product')
+    categories = relationship('Category', secondary='product_category')
 
-class Product_category(Base):
-    __tablename__ = 'product_category'
-    product_id = Column(ForeignKey('product.id'), primary_key =True)
-    category_id = Column(ForeignKey('category.id'), primary_key =True)
+
+class ProductCategory(Base):
+    __tablename__ = 'product_categories'
+    id = Column(Integer, primary_key=True)
+    product_id = Column(ForeignKey('products.id'))
+    category_id = Column(ForeignKey('categories.id'))
     extra_data = Column(String(100))
 
-    category = relationship("Category", backref = 'product_product_category')
-    product = relationship("Product", backref = 'category_product_category')
+    category = relationship('Category', backref='product_product_category')
+    product = relationship('Product', backref='category_product_category')
 
-
-    
 
 class Category(Base):
-    __tablename__ = 'category'
-    id = Column(Integer, primary_key = True)
+    __tablename__ = 'categories'
+    id = Column(Integer, primary_key=True)
     title = Column(String())
 
+
 class Basket(Base):
-    __tablename__ = 'basket'
-    id = Column(Integer, primary_key = True)
-    user_id = Column(Integer, ForeignKey('user.id'))
+    __tablename__ = 'baskets'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
     is_ordered = Column(Integer())
     total = Column(DECIMAL())
-    orders = relationship("Order")
-    products = relationship("Basket_product", backref = 'basket')
+    orders = relationship('Order')
+    products = relationship('Basket_product', backref='basket')
 
 
-
-class Basket_product(Base):
-    __tablename__ = 'basket_product'
-    id = Column(Integer, primary_key = True)
-    basket_id = Column(Integer, ForeignKey('basket.id'))
-    product_id = Column(Integer, ForeignKey('product.id'))
+class BasketProduct(Base):
+    __tablename__ = 'basket_products'
+    id = Column(Integer, primary_key=True)
+    basket_id = Column(Integer, ForeignKey('baskets.id'))
+    product_id = Column(Integer, ForeignKey('products.id'))
     quantity = Column(Integer())
     base_price = Column(Integer())
     final_price = Column(DECIMAL())
@@ -79,50 +82,49 @@ class Basket_product(Base):
 
 
 class Order(Base):
-    __tablename__ = 'order'
-    id = Column(Integer, primary_key = True)
-    basket_id = Column(Integer, ForeignKey('basket.id'))
-    user_id = Column(Integer, ForeignKey('user.id'))
-    deliveries = relationship("Delivery")
+    __tablename__ = 'orders'
+    id = Column(Integer, primary_key=True)
+    basket_id = Column(Integer, ForeignKey('baskets.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
+    deliveries = relationship('Delivery')
+
 
 class Delivery(Base):
-    __tablename__ = 'delivery'
-    id = Column(Integer, primary_key = True)
-    order_id = Column(Integer, ForeignKey('order.id'))
+    __tablename__ = 'deliveries'
+    id = Column(Integer, primary_key=True)
+    order_id = Column(Integer, ForeignKey('orders.id'))
     done = Column(Boolean())
-    address_id = Column(Integer, ForeignKey('address.id'))
-    pickup_point_id = Column(Integer, ForeignKey('pickup_point.id'))
+    address_id = Column(Integer, ForeignKey('addresses.id'))
+    pickup_point_id = Column(Integer, ForeignKey('pickup_points.id'))
 
-class Pickup_point(Base):
-    __tablename__ = 'pickup_point'
-    id = Column(Integer, primary_key = True)
+
+class PickupPoint(Base):
+    __tablename__ = 'pickup_points'
+    id = Column(Integer, primary_key=True)
     title = Column(String())
     city = Column(String())
     street = Column(String())
     house = Column(String())
-    deliveries = relationship("Delivery")
+    deliveries = relationship('Delivery')
+
 
 class Employer(Base):
-    __tablename__ = 'employer'
-    id = Column(Integer, primary_key = True)
+    __tablename__ = 'employees'
+    id = Column(Integer, primary_key=True)
     first_name = Column(String())
     last_name = Column(String())
     phone = Column(String(11))
-    email = Column(String(120), unique = True)
-    registred_day = Column(Date())
-    position_id = Column(Integer, ForeignKey('position.id'))
+    email = Column(String(120), unique=True)
+    register_day = Column(Date())
+    position_id = Column(Integer, ForeignKey('positions.id'))
 
 
 class Position(Base):
-    __tablename__ = 'position'
-    id = Column(Integer, primary_key = True)
+    __tablename__ = 'positions'
+    id = Column(Integer, primary_key=True)
     title = Column(String())
-    employers = relationship('employer')
-    
-
-
+    employers = relationship('Employer')
 
 
 if __name__ == '__main__':
-    Base.metadata.create_all(bind = engine)
-    
+    Base.metadata.create_all(bind=engine)
