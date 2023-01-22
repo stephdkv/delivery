26250d380d79775c9c0796f4bc1a164e86fdc9dc
abirukov <abirukov2008@gmail.com>
2023-01-22 13:59:52 +0300

    add review and slider actions

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -3,11 +3,14 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 
 from webapp.user.models import User
-from webapp.models import db
+from webapp.models import db, MainSliderAction, Review
 from webapp.user.views import blueprint as user_blueprint
 from webapp.admin.address.views import blueprint as address_blueprint
+from webapp.admin.category.views import blueprint as category_blueprint
 from webapp.admin.delivery.views import blueprint as delivery_b_blueprint
+from webapp.admin.order.views import blueprint as order_blueprint
 from webapp.admin.pickup_point.views import blueprint as pickup_point_blueprint
+from webapp.admin.product.views import blueprint as product_blueprint
 
 
 def create_app() -> Flask:
@@ -21,27 +24,23 @@ def create_app() -> Flask:
     login_manager.login_view = 'user.login'
 
     app.register_blueprint(user_blueprint)
-    app.register_blueprint(pickup_point_blueprint)
-    app.register_blueprint(delivery_b_blueprint)
     app.register_blueprint(address_blueprint)
+    app.register_blueprint(category_blueprint)
+    app.register_blueprint(delivery_b_blueprint)
+    app.register_blueprint(order_blueprint)
+    app.register_blueprint(pickup_point_blueprint)
+    app.register_blueprint(product_blueprint)
 
     @app.route("/")
     def index():
         title = 'Главная'
-        return render_template('index.html', carousel_items=[
-            {
-                'position': 0,
-                'heading': 'Тест1',
-                'description': 'Тест1  text',
-            },
-            {
-                'position': 1,
-                'heading': 'Тест2',
-                'description': 'Тест2  text',
-            },
-        ])
-
-
+        carousel_items = MainSliderAction.query.filter_by(is_active=True).order_by(MainSliderAction.position.asc()).all()
+        reviews = Review.query.filter_by(is_active=True).order_by(Review.id.asc()).all()
+        return render_template(
+            'index.html',
+            carousel_items=carousel_items,
+            reviews=reviews
+        )
 
     @login_manager.user_loader
     def load_user(user_id):
