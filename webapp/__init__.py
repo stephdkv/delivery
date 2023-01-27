80c52f4306040e9c50193e4a435cba538df04289
abirukov <abirukov2008@gmail.com>
2023-01-27 23:32:15 +0300

    fix basket

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,11 +2,9 @@ from flask import Flask, render_template, url_for, request
 from flask_login import LoginManager, current_user
 from flask_migrate import Migrate
 
-
 from webapp.forms import OrderingAddForm
 from webapp.models import OrderingForm
 from flask import flash
-
 
 from webapp.admin.category.models import Category
 from webapp.admin.product.models import Product, ProductCategory
@@ -69,16 +67,13 @@ def create_app() -> Flask:
         else:
             pass
 
-        
         return render_template('ordering.html', form=form)
-        
-
-
 
     @app.route("/")
     def index():
         title = 'Главная'
-        carousel_items = MainSliderAction.query.filter_by(is_active=True).order_by(MainSliderAction.position.asc()).all()
+        carousel_items = MainSliderAction.query.filter_by(is_active=True).order_by(
+            MainSliderAction.position.asc()).all()
         reviews = Review.query.filter_by(is_active=True).order_by(Review.id.asc()).all()
         categories = Category.query.filter_by(is_active=True).order_by(Category.id.asc()).all()
         products = Product.query.filter_by(is_active=True).order_by(Product.id.asc()).all()
@@ -92,8 +87,8 @@ def create_app() -> Flask:
             product_list=product_list,
         )
 
-    #@app.route("/basket")
-    #def your_basket():
+        # @app.route("/basket")
+        # def your_basket():
         title = 'Ваша корзина'
         basket_products = get_basket_list_with_product()
         product_ids = [x.product_id for x in basket_products]
@@ -112,40 +107,40 @@ def create_app() -> Flask:
         products = Product.query.filter(Product.id.in_(product_ids)).all()
         basket_products_list = []
         for basket_product in basket_products:
-            needle_product = False
+            needle_product = None
             for product in products:
-                if product.id == basket_product.product_id :
+                if product.id == basket_product.product_id:
                     needle_product = product
-                    #print(needle_product)
-            if not needle_product:
-                product_names = 'Не найден'
+                    # print(needle_product)
+            if needle_product is None:
+                product_name = 'Не найден'
             else:
-                product_names = needle_product.title
-                print(product_names)
-            for product_name in product_names:
-                basket_product_dict = {
-                        'id' : basket_product.id,
-                        'basket_id' : basket_product.basket_id,
-                        'product_id' : basket_product.product_id,
-                        'quantity' : basket_product.quantity,
-                        'base_price' : basket_product.base_price,
-                        'final_price' : basket_product.final_price,
-                        'is_active' : basket_product.is_active,
-                        'product_name': product_name
-                }
+                product_name = needle_product.title
+                print(product_name)
+            basket_product_dict = {
+                'id': basket_product.id,
+                'basket_id': basket_product.basket_id,
+                'product_id': basket_product.product_id,
+                'quantity': basket_product.quantity,
+                'base_price': basket_product.base_price,
+                'final_price': basket_product.final_price,
+                'is_active': basket_product.is_active,
+                'product_name': product_name
+            }
             basket_products_list.append(basket_product_dict)
             print(basket_products_list)
 
         return render_template(
             'basket.html',
-            basket_products_list = basket_products_list,
+            basket_products_list=basket_products_list,
 
         )
-    
+
     @app.route("/menu")
     def title_menu():
         title = 'Меню'
-        carousel_items = MainSliderAction.query.filter_by(is_active=True).order_by(MainSliderAction.position.asc()).all()
+        carousel_items = MainSliderAction.query.filter_by(is_active=True).order_by(
+            MainSliderAction.position.asc()).all()
         reviews = Review.query.filter_by(is_active=True).order_by(Review.id.asc()).all()
         categories = Category.query.filter_by(is_active=True).order_by(Category.id.asc()).all()
         products = Product.query.filter_by(is_active=True).order_by(Product.id.asc()).all()
@@ -158,13 +153,12 @@ def create_app() -> Flask:
             categories=categories,
             product_list=product_list,
         )
-    
+
     @app.route('/product_add')
     def product_add():
         return render_template(
             'basket.html'
         )
-
 
     def get_category_dict_by_id(category_id: int, categories: list) -> dict | None:
         for category in categories:
@@ -200,9 +194,6 @@ def create_app() -> Flask:
             product_list.append(product_dict)
         return product_list
 
-    
-
-
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(user_id)
@@ -210,13 +201,12 @@ def create_app() -> Flask:
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(user_id)
-
 
     def get_basket_list_with_product():
         basket = Basket.query.filter(
-        current_user.id == Basket.user_id,
-        Basket.is_ordered == False,
-    ).first()
+            current_user.id == Basket.user_id,
+            Basket.is_ordered == False,
+        ).first()
         if basket is not None:
             basket_products = BasketProduct.query.filter_by(basket_id=basket.id).all()
         return basket_products
